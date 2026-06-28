4ef5fbdb9dca0ae8eb218f02e58a8e358637913a
vikasreddy11 <vikasreddy1090@gmail.com>
2026-06-28 16:12:34 +0530

    Added frontend

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className=''>
+      <div className='bg-cyan-100 h-20'>
+        <div>Restate</div>
+      </div>
+   </div>
+  )
+}
+
+export default App
