7c616091570611fec2f3d6e16ae7e1c72a6a1d4b
Srikar Gadagoju <you@example.com>
2026-06-29 14:25:30 +0530

    added components

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { SearchCard } from './components/SearchCard'
 import { Home } from './pages/home'
 
 
@@ -6,7 +7,7 @@ function App() {
 
   return (
     <div className=''>
-      <Home/>
+      <SearchCard/>
    </div>
   )
 }
